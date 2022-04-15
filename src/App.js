0eea91ee77c1a6aa5adab26f9e96e3132558823a
Rustin Dodd <rustin.dodd@ibm.com>
2022-04-15 14:07:20 -0500

    added slick slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Slider from "react-slick";
+
+// Import styles
 import './css/app.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 // Import the data class
 import Data from './data';
@@ -26,6 +31,14 @@ class App extends React.Component {
   }
 
   render() {
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     
     return (
       <div className="App">
@@ -37,6 +50,26 @@ class App extends React.Component {
           {this.state.articles.map((article)=> (
             <li key={article.id}>{ article.title }</li>
           ))}
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
         </header>
         }
       </div>
