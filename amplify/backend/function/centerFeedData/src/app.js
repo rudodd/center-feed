d49d6c2101fa855929d25785dcdf1bd75cf945fd
Rustin Dodd <rustin.dodd@ibm.com>
2022-04-19 12:44:32 -0500

    added backend to handle API requests

---
+++ amplify/backend/function/centerFeedData/src/app.js
@@ -0,0 +1,154 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+const request = require('request')
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+
+
+app.get('/feed/allSidesSources', function(req, res) {
+  request('https://www.allsides.com/media-bias/json/noncommercial/publications', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json({
+        success: 'Successfully fetched AllSides Media sources',
+        code: response.statusCode,
+        url: req.url,
+        data: body,
+      });
+    } else if (error) {
+      res.json({
+        error: `Error fetching AllSides Media sources: ${error}`,
+        code: response.statusCode,
+        url: req.url,
+      });
+    }
+  })
+});
+
+app.post('/feed/newsAPISources', function(req, res) {
+  request(`https://newsapi.org/v2/sources?apiKey=${req.body.key}`, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json({
+        success: 'Successfully fetched News API sources',
+        code: response.statusCode,
+        url: req.url,
+        data: body,
+      });
+    } else {
+      res.json({
+        error: `Error fetching News API sources: ${error}`,
+        code: response.statusCode,
+        response: response,
+        url: req.url,
+      });
+    }
+  })
+});
+
+app.post('/feed/newsAPIArticles', function(req, res) {
+  request(`https://newsapi.org/v2/top-headlines?sources=${req.body.sources}&apiKey=${req.body.key}&pageSize=100`, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json({
+        success: 'Successfully fetched News API articles',
+        code: response.statusCode,
+        url: req.url,
+        data: body,
+      });
+    } else {
+      res.json({
+        error: `Error fetching News API articles: ${error}`,
+        code: response.statusCode,
+        url: req.url,
+      });
+    }
+  })
+});
+
+// /**********************
+//  * Example get method *
+//  **********************/
+
+//  app.get('/feed', function(req, res) {
+//   // Add your code here
+//   res.json({
+//     success: 'get call succeed!', 
+//     url: req.url,
+//     test: 'testing',
+//   });
+// });
+
+// /****************************
+// * Example post method *
+// ****************************/
+
+// app.post('/item', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'post call succeed!', url: req.url, body: req.body})
+// });
+
+// app.post('/item/*', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'post call succeed!', url: req.url, body: req.body})
+// });
+
+// /****************************
+// * Example put method *
+// ****************************/
+
+// app.put('/item', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'put call succeed!', url: req.url, body: req.body})
+// });
+
+// app.put('/item/*', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'put call succeed!', url: req.url, body: req.body})
+// });
+
+// /****************************
+// * Example delete method *
+// ****************************/
+
+// app.delete('/item', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'delete call succeed!', url: req.url});
+// });
+
+// app.delete('/item/*', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'delete call succeed!', url: req.url});
+// });
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
