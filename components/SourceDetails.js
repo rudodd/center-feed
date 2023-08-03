4d643c26edf8e08f573fd0e5dc1e8784303fcc87
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-03 15:13:33 -0500

    converted source details into functional component

--- components/SourceDetails.js
+++ components/SourceDetails.js
@@ -1,42 +1,39 @@
+// import library functionality
 import React from "react";
+
+// import components
 import Link from 'next/link';
 
+export default function SourceDetails(props) {
 
-class SourceDetails extends React.Component {
-
-  render() {
-
-    const { source } = this.props;
-    const sourceLogo = `/img/sources/${source.id}.png`;
-    let leanClass;
-    let leanText;
-    switch(source.lean) {
-      case 'Lean Left':
-        leanClass = 'left';
-        leanText = 'Center Left'
-        break;
-      case 'Lean Right':
-        leanClass = 'right';
-        leanText = 'Center Right';
-        break;
-      default:
-        leanClass = 'center';
-        leanText = 'Center';
-    }
-
-    return (
-      <div className="source-details">
-        <div className="source-logo">
-          <Link href={ source.url } target="_blank" rel="noreferrer">
-          <img src={ sourceLogo } alt={ source.name } />
-          </Link>
-        </div>
-        <div className="lean-info">
-          <Link href={ source.allSidesURL } target="_blank" rel="noreferrer">Source leans: <span className={ leanClass }>{ leanText }</span></Link>
-        </div>
-      </div>
-    )
+  const { source } = props;
+  const sourceLogo = `/img/sources/${source.id}.png`;
+  let leanClass;
+  let leanText;
+  switch(source.lean) {
+    case 'Lean Left':
+      leanClass = 'left';
+      leanText = 'Center Left'
+      break;
+    case 'Lean Right':
+      leanClass = 'right';
+      leanText = 'Center Right';
+      break;
+    default:
+      leanClass = 'center';
+      leanText = 'Center';
   }
-}
 
-export default SourceDetails;
+  return (
+    <div className="source-details">
+      <div className="source-logo">
+        <Link href={ source.url } target="_blank" rel="noreferrer">
+        <img src={ sourceLogo } alt={ source.name } />
+        </Link>
+      </div>
+      <div className="lean-info">
+        <Link href={ source.allSidesURL } target="_blank" rel="noreferrer">Source leans: <span className={ leanClass }>{ leanText }</span></Link>
+      </div>
+    </div>
+  )
+}
