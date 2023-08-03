d27f1b1e287ba36b40acb9f34d37afc3d8b21e4e
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-03 14:48:53 -0500

    converted article wrapper to functional component

--- components/ArticleWrapper.js
+++ components/ArticleWrapper.js
@@ -1,27 +1,23 @@
+// import library functionality
 import React from "react";
 
 // Import components
 import SmallArticle from "./SmallArticle";
 import ArticleSlider from "./ArticleSlider";
 
-class ArticleWrapper extends React.Component {
+export default function ArticleWrapper(props){
 
-  render() {
+  const { article, sources } = props;
+  const source = sources.verbose.filter(source => source.id === article.source.id)[0];
 
-    const { article, sources } = this.props;
-    const source = sources.verbose.filter(source => source.id === article.source.id)[0];
-
-    return (
-      <div className="article-wrap">
-        {article.relatedArticles.length === 0 &&
-          <SmallArticle article={article} source={source} />
-        }
-        {article.relatedArticles.length !== 0 &&
-          <ArticleSlider article={article} sources={sources} />
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="article-wrap">
+      {article.relatedArticles.length === 0 &&
+        <SmallArticle article={article} source={source} />
+      }
+      {article.relatedArticles.length !== 0 &&
+        <ArticleSlider article={article} sources={sources} />
+      }
+    </div>
+  )
 }
-
-export default ArticleWrapper;
