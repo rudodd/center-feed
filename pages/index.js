da9b649a512be48e76b11d194af6ef63d6af7538
Rustin Dodd <rustin.dodd@ibm.com>
2024-09-24 15:28:58 -0500

    npm audit fix and unavailable message

--- pages/index.js
+++ pages/index.js
@@ -12,37 +12,37 @@ import Footer from '../components/Footer';
 
 export default function Home() {
 
-  const data = new Data();
-  const [loading, setLoading] = useState(true);
-  const [hideNotification, setHideNotification] = useState(false);
-  const [sources, setSources] = useState(null);
-  const [articles, setArticles] = useState(null);
+  // const data = new Data();
+  const [loading, setLoading] = useState(false);
+  // const [hideNotification, setHideNotification] = useState(false);
+  // const [sources, setSources] = useState(null);
+  // const [articles, setArticles] = useState(null);
 
-  const getData = () => {
-    if (!loading) {
-      setLoading(true);
-    }
-    data.getData()
-      .then((feed)=> {
-        setSources(JSON.parse(feed.sources));
-        setArticles(JSON.parse(feed.articles));
-      });
-  }
+  // const getData = () => {
+  //   if (!loading) {
+  //     setLoading(true);
+  //   }
+  //   data.getData()
+  //     .then((feed)=> {
+  //       setSources(JSON.parse(feed.sources));
+  //       setArticles(JSON.parse(feed.articles));
+  //     });
+  // }
 
-  useEffect(() => {
-    getData();
-  }, [])
+  // useEffect(() => {
+  //   getData();
+  // }, [])
 
-  useEffect(() => {
-    if (!empty(sources) && !empty(articles)) {
-      setLoading(false);
-    }
-  })
+  // useEffect(() => {
+  //   if (!empty(sources) && !empty(articles)) {
+  //     setLoading(false);
+  //   }
+  // })
     
   return (
     <div className="app">
-      <Header loading={loading} refresh={getData}/>
-      {!hideNotification &&
+      <Header loading={loading} refresh={null}/>
+      {/* {!hideNotification &&
         <div className="desktop-notification">
           <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M11 17h2v-6h-2Zm1-8q.425 0 .713-.288Q13 8.425 13 8t-.287-.713Q12.425 7 12 7t-.712.287Q11 7.575 11 8t.288.712Q11.575 9 12 9Zm0 13q-2.075 0-3.9-.788-1.825-.787-3.175-2.137-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175 1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138 1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175-1.35 1.35-3.175 2.137Q14.075 22 12 22Zm0-2q3.35 0 5.675-2.325Q20 15.35 20 12q0-3.35-2.325-5.675Q15.35 4 12 4 8.65 4 6.325 6.325 4 8.65 4 12q0 3.35 2.325 5.675Q8.65 20 12 20Zm0-8Z"/></svg>
           <p>This app was designed for mobile.  For the best experience, visit this site on a mobile device.</p>
@@ -60,7 +60,11 @@ export default function Home() {
             <ArticleWrapper key={article.id} article={article} sources={sources} />
           ))}
         </div>
-      }
+      } */}
+      <div className="app-container">
+        <h1>App unavailable</h1>
+        <p>Due to issues of support from the APIs from which we pull new sources and media ratings, this app is temporarily unavailable.  We are working to find alternative APIs in order to continue providing this service.</p>
+      </div>
       {!loading &&
         <Footer/>
       }
