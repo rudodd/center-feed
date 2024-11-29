cc90999fdc469cbbed32c348d985f6842c86d9ec
Rustin Dodd <rustin.dodd@ibm.com>
2024-11-29 10:35:21 -0600

    addes back in the political bias API and updates README

--- README.md
+++ README.md
@@ -1,22 +1,12 @@
 # The Center Feed
 
-The center feed is an app designed for mobile use that displays current news from only center leaning, center-left leaning and center-right leaning news organizations.  It does this by fetching the current news from [NewsAPI](https://newsapi.org/) and filtering it down to only the news organizations that are rated as center, center-left, or center-right by [All Sides Media](https://www.allsides.com/).
+The center feed is an app designed for mobile use that displays current news from only center leaning, center-left leaning, and center-right leaning news organizations.  It does this by fetching the current news from [OkSurf News API](https://ok.surf/) and filtering it down to only the news organizations that are rated as center, center-left, or center-right by the [Political Bias Database](https://rapidapi.com/albertoescobar/api/political-bias-database).
 
 Live app (designed for use on mobile): [centerfeed.org](https://www.centerfeed.com)
-
-__NOTE:__ Unfortunately NewsAPI changed their API's free plan to only return articles on a 24 hour delay, which used to be a 15 minute delay.  I have not found a suitable free substitute, but would love any suggestions.
-
-## Features
-- Real time news articles from center leaning sources (as mentioned above, this is now on a 24 hour delay)
-- Related article grouping and hot topic indication
-- Link on each article to information regarding how the article's source leans
 
 ## Getting Started
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
-It uses minimal additional npm packages, but those used are as follows:
-- [Slick Carousel](https://www.npmjs.com/package/slick-carousel) - Used for the mobile friendly slider for related articles
 
 To run the development server:
 
--- pages/api/all-sides-sources.js
+++ pages/api/all-sides-sources.js
@@ -1,5 +1,4 @@
 import clientPromise from "../../lib/mongodb";
-import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
@@ -7,43 +6,41 @@ export default async function handler(req, res) {
   const sources = await db.collection("sources").find({}).toArray();
   const today = new Date().getTime();
   const expireDate = new Date(sources[0].expire).getTime();
-  // if (today > expireDate) {
-  //   console.log('fetch new sources');
-  //   const expires = new Date();
-  //   expires.setDate(expires.getDate() + 7);
-  //   const options = {
-  //     method: 'GET',
-  //     hostname: 'political-bias-database.p.rapidapi.com',
-  //     port: null,
-  //     path: '/',
-  //     headers: {
-  //       'x-rapidapi-key': 'f096819199msh539dfcd3b2394c1p128b3cjsn2307ae796a4c',
-  //       'x-rapidapi-host': 'political-bias-database.p.rapidapi.com'
-  //     }
-  //   };
-  //   fetch('https://political-bias-database.p.rapidapi.com/ASdata', options)
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json()
-  //           .then((json) => {
-  //             const updated = {sources: json, expire: expires}
-  //             db.collection("sources").updateOne(
-  //               {id: 1},
-  //               {
-  //                 $set: {...updated},
-  //                 $currentDate: { lastModified: true }
-  //               }
-  //             )
-  //             .then(() => {
-  //               return res.status(200).json(json);
-  //             })
-  //           })
-  //       } else {
-  //         return res.status(200).json(sources[0].sources);
-  //       }
-  //     })
-  // } else {
-  //    return res.status(200).json(sources[0].sources);  
-  // }
-  return res.status(200).json(sources[0].sources);
+  if (today > expireDate) {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    const options = {
+      method: 'GET',
+      hostname: 'political-bias-database.p.rapidapi.com',
+      port: null,
+      path: '/',
+      headers: {
+        'x-rapidapi-key': process.env.ALL_SIDES_KEY,
+        'x-rapidapi-host': 'political-bias-database.p.rapidapi.com'
+      }
+    };
+    fetch('https://political-bias-database.p.rapidapi.com/ASdata', options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+            .then((json) => {
+              const updated = {sources: json, expire: expires}
+              db.collection("sources").updateOne(
+                {id: 1},
+                {
+                  $set: {...updated},
+                  $currentDate: { lastModified: true }
+                }
+              )
+              .then(() => {
+                return res.status(200).json(json);
+              })
+            })
+        } else {
+          return res.status(200).json(sources[0].sources);
+        }
+      })
+  } else {
+     return res.status(200).json(sources[0].sources);  
+  }
 }
